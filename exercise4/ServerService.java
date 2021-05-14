1fa532d79dcecbe3a5ad7f701e34b5ba3cca3e89
lucyphuongnhu <truonghoangphuongnhu@gmail.com>
2021-05-14 19:25:49 +0700

    Delete exercise4 directory

--- exercise4/ServerService.java
+++
@@ -1,9 +0,0 @@
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-import java.util.List;
-
-public interface ServerService extends Remote {
-    public List<Media> getMedia(String type) throws RemoteException;
-
-    public void createMedia(String type, String name, String author) throws RemoteException;
-}
